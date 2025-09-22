e87ac73c058d1d7df6912ff079dc7a73d744ad36
akshaymishra-0 <akshaymishra2004s@gmail.com>
2025-09-22 15:13:01 +0530

    Fix Vercel deployment configuration

---
+++ index.js
@@ -0,0 +1,2 @@
+// This file helps Vercel find the main server entry point
+module.exports = require('./server/index.js');
--- server/index.js
+++ server/index.js
@@ -17,10 +17,11 @@ app.use('/api/compiler', compilerRoutes);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  const staticPath = path.join(__dirname, '../client/build');
+  app.use(express.static(staticPath));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(staticPath, 'index.html'));
   });
 } else {
   // Base route for development
